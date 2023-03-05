3b6902890609574ed4adeee31aaf3b437b346680
Andrey Yurov <a.yurov6@gmail.com>
2023-03-06 00:57:40 +0700

    Replaced include on require

--- addProduct/submit.php
+++ addProduct/submit.php
@@ -1,5 +1,5 @@
 <?php
-  include '../config/config.php';
-  require_once $_SERVER['DOCUMENT_ROOT'].'/autoload.php';
+  require '../config/config.php';
+  require $_SERVER['DOCUMENT_ROOT'] . '/autoload.php';
   $db = new classes\Database();
   classes\Product::saveProduct($db, filter_input_array(INPUT_POST));
